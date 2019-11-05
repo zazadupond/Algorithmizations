14203c6311ccde5d1981d4a9d993aa6f36ef77fa
zazadupond <pocketbookjava@gmail.com>
2019-11-05 15:39:50 +0300

    Task1

---
+++ src/by/epam/stady/OneDimensionAlarray/Task3.java
@@ -0,0 +1,34 @@
+package by.epam.stady.OneDimensionAlarray;
+
+/* Дан массив действительных чисел, размерность которого N. Посчитать, сколько в нем
+* отрицательных, положительных и нулевых элементов*/
+
+public class Task3 {
+
+    public static void main(String[] args) {
+
+        int n = (int) (1 + Math.random() * 100);
+        int[] array = new int[n];
+        int negative = 0;
+        int positive = 0;
+        int zero = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (-50 + Math.random() * 100);
+            if (array[i] > 0) {
+                positive++;
+            } else if (array[i] < 0) {
+                negative++;
+            } else if (array[i] == 0) {
+                zero++;
+            }
+
+        }
+
+        System.out.println(" В массиве размером N = "+ n + " присутствуют элементы:" +
+                " \n положительные " + positive +
+                " \n отрицательные " + negative +
+                " \n нулевые " + zero);
+
+    }
+}
