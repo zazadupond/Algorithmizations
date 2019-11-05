b5ea7020a799c122ba3d7a05be4c2431615f6f77
zazadupond <pocketbookjava@gmail.com>
2019-11-06 00:09:20 +0300

    Task6

---
+++ src/by/epam/stady/OneDimensionAlarray/Task6.java
@@ -0,0 +1,29 @@
+package by.epam.stady.OneDimensionAlarray;
+
+/* Задана последовательность N вещественных чисел. Вычислить сумму чисел,
+* подядковые номера которых являются простыми числами.*/
+
+public class Task6 {
+    public static void main(String[] args) {
+
+        int summat = 0;
+        int[] array = new int[(int) (1 + Math.random() * 200)]; // задаем величину массива от 1 до 200
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 201) - 100; // инициируем массив цислами от -100 до 100 включительно
+
+            double s = Math.sqrt(i);
+            for (int j = 2 ; j <= s; j++){
+                if ((i % j) == 0) {
+                    System.out.println("Не простое ");
+                    break;
+                } else {
+                    summat += array[i];
+                    System.out.println("Добавляем  " + array[i]);
+                }
+            }
+
+        }
+        System.out.println("Summator " + summat);
+    }
+}
