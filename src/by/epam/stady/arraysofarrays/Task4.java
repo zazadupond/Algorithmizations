56f70e8cdfa69acf17af35b6d41ddcd83a297c7a
zazadupond <pocketbookjava@gmail.com>
2019-11-12 02:25:24 +0300

    Task4

---
+++ src/by/epam/stady/arraysofarrays/Task4.java
@@ -0,0 +1,41 @@
+package by.epam.stady.arraysofarrays;
+
+/* Сформировать квадратную матрицу порядка n по заданному образцу (n- четное)*/
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Task4 {
+
+    public static void main(String[] args) {
+        int n = 8;
+        int[][] array = createMatrixOnTheModel(n, n);
+
+        for ( int i = 0; i < array.length; i++){
+            for (int j = 0; j < array[i].length; j++){
+                System.out.print(array[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] createMatrixOnTheModel(int rows, int columns) {
+
+        int[][] array = new int[rows][columns];
+
+        for (int i = 0; i < array.length; i++){
+            if (i % 2 == 0) {
+                for (int j = 0; j < array[i].length; j++) {
+                    array[i][j] = j + 1;
+                }
+            } else {
+                for (int z = 0; z < array[i].length; z++) {
+                    array[i][z] = array[i].length - z;
+                }
+            }
+        }
+
+        return array;
+    }
+
+}
