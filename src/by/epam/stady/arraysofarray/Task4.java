d7eeb3f6620efeb28ec8d2d09b16e2047fda0982
zazadupond <mTsy7H7494vQNdTnUoTw>
2020-04-06 18:07:57 +0300

    rename package

--- src/by/epam/stady/arraysofarray/Task4.java
+++ src/by/epam/stady/arraysofarray/Task4.java
@@ -1,17 +1,5 @@
 package by.epam.stady.arraysofarrays;
 
-/* Сформировать квадратную матрицу порядка n по заданному образцу (n- четное)
-* 1  2   3  ... n
-* n n-1 n-2  ...1
-* 1  2   3  ... n
-* n n-1 n-2  ...1
-* .   .  .   ....
-* .   .  .      .
-* n n-1 n-2  ...1 */
-
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 public class Task4 {
 
     public static void main(String[] args) {
